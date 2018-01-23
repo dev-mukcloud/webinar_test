936dcd9d8ff63f13ff9e3796e63a40af552e9f29
dev-mukcloud <32312166+dev-mukcloud@users.noreply.github.com>
2018-01-23 06:13:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,9 +6,9 @@ echo "<title>23 January 2018</title>";
 echo "<meta charset=\"utf-8\" />";
 echo "</head>";
 echo "<body>";
-echo "<H1> Hello world! Test GIT </H1>";
+echo "<H1> Hello Webinar! Test GIT </H1>";
 echo "<p>";
-while ($i< 100)
+while ($i< 1000)
 {
   print "i".$i++.";";
 }
