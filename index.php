0a2fc63c391a21037cb7c6d84b083b85bb50dfcb
dev-mukcloud <32312166+dev-mukcloud@users.noreply.github.com>
2018-01-23 06:07:05 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+echo "<!DOCTYPE html>";
+echo "<html>";
+echo "<head>";
+echo "<title>23 January 2018</title>";
+echo "<meta charset=\"utf-8\" />";
+echo "</head>";
+echo "<body>";
+echo "<H1> Hello world! Test GIT </H1>";
+echo "<p>";
+while ($i< 100)
+{
+  print "i".$i++.";";
+}
+echo "</p>";
+  
+echo "</body>";
+echo "</html>";
+?>
